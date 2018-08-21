b957442101415a2634d038ee01c9d903b693370d
Marcin Rozok <marcin.rozok@performgroup.com>
2018-08-21 16:38:57 +0200

    Introducing missing scope;

--- app/src/main/java/pl/mrozok/daggerarchitecture/injection/common/UtilModule.kt
+++ app/src/main/java/pl/mrozok/daggerarchitecture/injection/common/UtilModule.kt
@@ -3,10 +3,12 @@ package pl.mrozok.daggerarchitecture.injection.common
 import dagger.Module
 import dagger.Provides
 import pl.mrozok.daggerarchitecture.common.Logger
+import javax.inject.Singleton
 
 @Module
 class UtilModule {
 
+    @Singleton
     @Provides
     fun provideLogger(): Logger = Logger()
 }
